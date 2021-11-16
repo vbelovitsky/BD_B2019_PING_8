55832d589359a93be2a0b6f89ff1a2ba07f9dd1a
Shalaeva Marina <mashalaeva@edu.hse.ru>
2021-11-16 19:10:58 +0300

    Added correct project directory

---
+++ Practice 8/Шалаева Марина/LibraryDB/LibraryDB.Domain/Publisher.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryDB.Domain
+{
+    public class Publisher
+    {
+        // Имя издательства.
+        [Key] public string PubName { get; set; }
+
+        // Адрес издательства.
+        public string PubAddress { get; set; }
+    }
+}
